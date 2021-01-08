134998fdc0d4b9a1551c94233aa9e9d9719ad6ff
Sriram <sriam99@gmail.com>
2021-01-08 13:47:07 +0530

    added services and Observables

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes'
+import { HeroService } from "../hero.service"
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,12 +11,16 @@ import { HEROES } from '../mock-heroes'
 export class HeroesComponent implements OnInit {
   // hero: Hero = { id: 1, name: 'WindStorm' }
   selectedHero!: Hero;
-  heroes = HEROES
-  constructor() {
+  heroes !: Hero[]
+  constructor(private heroService: HeroService) {
     // this.selectedHero = { id: 0, name: "" }
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
   ngOnInit(): void {
+    this.getHeroes()
   }
 
   onSelect(hero: Hero): void {
