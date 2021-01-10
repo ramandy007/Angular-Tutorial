3c391dc18c432c558c4ad283b31e31c2636d380b
Sriram <sriam99@gmail.com>
2021-01-10 12:42:57 +0530

    in memory data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,14 +3,21 @@ import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 import { Observable, of } from 'rxjs'
 import { MessageService } from "./message.service"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+  // getHeroes(): Observable<Hero[]> {
+  //   this.messageService.add("HeroService:fetched heroes")
+  //   return of(HEROES)
+  // }
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add("HeroService:fetched heroes")
-    return of(HEROES)
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
+
   getHero(id: number | string | null): Observable<Hero | undefined> {
 
     this.messageService.add(`HeroService:Fetched hero id = ${id}`)
@@ -18,5 +25,9 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id))
 
   }
-  constructor(private messageService: MessageService) { }
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 }
